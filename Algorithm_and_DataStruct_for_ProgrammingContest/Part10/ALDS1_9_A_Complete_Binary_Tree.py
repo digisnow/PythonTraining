c3f8a012898f1e40f7af8086ced52a1559cab632
digisnow <y.aki.taji.1003@gmail.com>
2019-08-01 20:54:33 +0900

    add complete binary tree

---
+++ Algorithm_and_DataStruct_for_ProgrammingContest/Part10/ALDS1_9_A_Complete_Binary_Tree.py
@@ -0,0 +1,25 @@
+MAX = 100000
+
+def parent(i):
+    return i // 2
+
+def left(i):
+    return  2 * i
+
+def right(i):
+    return 2 * i + 1
+
+H = int(input())
+A = list(map(int,input().split()))
+
+A.insert(0, 0)
+
+for i in range(1, H+1):
+    print("node " + str(i) + ": key = " + str(A[i]) + ", ", end="")
+    if parent(i) >= 1:
+        print("parent key = " + str(A[parent(i)]) + ", ",end="")
+    if left(i) <= H:
+        print("left key = " + str(A[left(i)]) + ", ",end="")
+    if right(i) <= H:
+        print("right key = " + str(A[right(i)]) + ", ",end="")
+    print()
